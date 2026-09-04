708efc46982371714ad1bceb3b52bffd39db4459
Lukas Kayo <lukaskayo3@gmail.com>
2026-09-04 20:20:29 -0300

    Create 17 - list.dart

---
+++ 17 - list.dart
@@ -0,0 +1,12 @@
+void main() {
+
+
+ 
+ List<String> alunos = ['Adryan', 'Alan', 'Lukas', 'João'];
+ print(alunos);
+ alunos.add ('Bruna');
+ print(alunos);
+ alunos.remove ('Alan');
+ print(alunos);
+ print(alunos[0]);
+}
