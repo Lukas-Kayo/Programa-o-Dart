e78bebb3d716df8a254e5c5c5ce0b93b6f13f4fd
Lukas Kayo <lukaskayo3@gmail.com>
2026-09-04 20:35:51 -0300

    Create 19 - Map.dart

---
+++ 19 - Map.dart
@@ -0,0 +1,14 @@
+void main() {
+
+
+ 
+ Map<String , dynamic> alunos = {
+ 'nome' : 'Bruna',
+ 'idade' : '20',
+ 'nota' : '7.0',
+ };
+
+ print(alunos['nome']);
+ alunos['idade'] =22;
+ print(alunos);
+}
