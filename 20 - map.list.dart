ee0bc5b12ba7b01bd61428f0bf91075bbe571c38
Lukas Kayo <lukaskayo3@gmail.com>
2026-09-04 20:50:34 -0300

    Update 20 - map.list.dart

--- 20 - map.list.dart
+++ 20 - map.list.dart
@@ -2,6 +2,11 @@ void main() {
   
 
  final numeros = [1, 2, 3, 4, 5, 6, 7];
+  
  final dobrados = numeros.map((n) => n * 2).toList();
  print(dobrados);
-}  
+
+final pares = numeros.where((n) => n.isEven).toList();
+print(pares);
+
+}           
